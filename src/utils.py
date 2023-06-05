744a5253e13822ff04c1a13dd0983549efeb66a0
RySE-AI <Ryo.Shibata@outlook.de>
2023-06-05 14:26:57 +0200

    feat: Add functions to get dataset details

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,11 @@
-import torch
+import pathlib
 import logging
+from collections import Counter
+
+import cv2
+import numpy as np
+import torch
+
 from .custom_datasets import MultiLabelImageFolder
 
 def check_gpu_available() -> torch.device:
@@ -43,4 +49,36 @@ def print_dataset_information(dataset_dir: str):
         print(f"{i}: {cls_combi}")
         
     print("\nThese represents the possible class combinations in your dataset")
-    print(50*"-")    
+    print(50*"-")
+    
+
+
+def check_img_shapes(image_folder: str,
+                     ext = ".jpg") -> Counter:
+    image_paths = pathlib.Path(image_folder).rglob(f"**/*{ext}")
+    shapes = [cv2.imread(str(img_path)).shape[:2] for img_path in image_paths]
+    
+    return Counter(shapes)
+
+#TODO: 
+def calculate_mean_stds_per_channel(image_folder,
+                                    ext = ".jpg"):
+    channels_mean = np.zeros(3)
+    channels_std = np.zeros(3)
+
+    image_paths = pathlib.Path(image_folder).rglob(f"**/*{ext}")
+    
+    if not image_paths:
+        raise ValueError("There no images in the given folder")
+    
+    for count ,image_path in enumerate(image_paths, 1):
+        image = cv2.imread(str(image_path))
+        image = image.astype(np.float32) / 255.0  # Normalize pixel values
+
+        channels_mean += np.mean(image, axis=(0, 1))
+        channels_std += np.std(image, axis=(0, 1))
+
+    channels_mean /= count
+    channels_std /= count
+
+    return channels_mean[::-1], channels_std[::-1] #rearrange for RGB Order
