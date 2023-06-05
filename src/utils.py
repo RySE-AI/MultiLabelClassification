6a16d70164560cee1c89684827ff4046328cd06c
RySE-AI <Ryo.Shibata@outlook.de>
2023-06-05 14:25:54 +0200

    feat: Add function to print dataset info

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 import torch
 import logging
-
+from .custom_datasets import MultiLabelImageFolder
 
 def check_gpu_available() -> torch.device:
     """This function checks which hardware backend is available on your device
@@ -26,3 +26,21 @@ def check_gpu_available() -> torch.device:
         logging.info("Running on CPU")
 
     return device
+
+
+def print_dataset_information(dataset_dir: str):
+    image_dataset = MultiLabelImageFolder(dataset_dir)
+    
+    classes = ", ".join(image_dataset.classes)
+    
+    print("Dataset Summary")
+    print(50*"-")
+    print(f"\nThe dataset contains {len(image_dataset)} images")
+    print(f"The possible classes are:\n{classes.title()}")
+    print(f"\nThe folders are structured as followed:\n")
+    for i, cls_combi in enumerate(image_dataset.cls_combinations):
+        cls_combi = " and ".join(cls_combi)
+        print(f"{i}: {cls_combi}")
+        
+    print("\nThese represents the possible class combinations in your dataset")
+    print(50*"-")    
